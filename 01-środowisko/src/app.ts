d80a60780e526b5a0c7d19d7940f53167153107a
Maja Marchewka <marchewka.maja@gmail.com>
2017-07-24 21:32:47 +0200

    todo app: basic functions

--- 01-środowisko/src/app.ts
+++ 01-środowisko/src/app.ts
@@ -10,5 +10,45 @@ console.log("Hello, world!");
 const greetMaja = new Greeter('Maja');
 greetMaja.greet();
 
-
 //$('body').html('Hey z jQuery');
+
+document.addEventListener('DOMContentLoaded', onStart);
+
+const tasks:Task[] = loadTasks();
+
+interface Task {
+    text: string;
+    state: 'active'|'completed';
+}
+
+function onStart() {
+    const button = document.getElementById('addButton') as HTMLButtonElement;
+    button.addEventListener('click', addTask);
+    renderTasks();
+    
+}
+
+function addTask(){
+    const textField = document.getElementById('textField') as HTMLInputElement;
+    const text = textField.value;
+    tasks.push({text: text, state: 'active'});
+    saveTasks();
+    renderTasks();
+}
+
+function renderTasks() {
+    const listContainer = document.getElementById('list') as HTMLDivElement;
+    listContainer.innerHTML = '';
+    for(let i=0; i<tasks.length; i++) {
+        listContainer.innerHTML += '<div>' + tasks[i].text + ', ' + tasks[i].state + '</div>';
+    }
+}
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    return JSON.parse(localStorage.getItem('tasks') || '[]');
+     
+}
